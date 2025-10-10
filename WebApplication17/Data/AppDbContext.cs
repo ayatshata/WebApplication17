f91b4c8307bc7a5b6e76c13d64fc6427b560d897
skynet <skynet@AYATALI>
2025-10-10 15:37:46 -0700

    Add project files.

---
+++ WebApplication17/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication17.Models;
+
+namespace WebApplication17.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+       
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Ayat Ali", Email = "yuy@example.com", CreatedAt = DateTime.Now },
+                new User { Id = 2, Name = "Ali Ahmed", Email = "ali@example.com", CreatedAt = DateTime.Now },
+                               new User { Id = 3, Name = "Ahmed Ali", Email = "ahe@example.com", CreatedAt = DateTime.Now }
+
+
+            );
+        }
+    }
+}
---
+++ WebApplication17/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using WebApplication17.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.EnsureCreated(); 
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
+    }
+}
+
+app.Run();
